7ccead955b3a9d35d1713f101e113cb5eadfa4b1
Gskartwii <agoldstar101@gmail.com>
2014-02-11 17:56:42 +0200

    Added brsar.c

---
+++ brsar.c
@@ -0,0 +1,52 @@
+// BRSAR offsets ~ Made by GSKW
+
+
+/*==============================================================================*/
+/*=========================== New Courses/Arenas ===============================*/
+/*==============================================================================*/
+#define N_BLOCK_F		0xF929C		// Block Plaza ~ Last minute
+#define N_BLOCK_N		0xFE968		// Block Plaza ~ Normal
+#define N_BOSSMI_32		0xFFD80		// Galaxy Colosseum/Arena
+#define N_CASINO_F		0xF76EC		// Chain Chomp Roulette/Wheel ~ Last minute
+#define N_CASINO_N		0xF7724		// Chain Chomp Roulette/Wheel ~ Normal
+#define N_CIRCUIT32_F	0xFCAF8		// Luigi/Mario Circuit ~ Last Lap
+#define N_CIRCUIT32_N	0xFC930		// Luigi/Mario Circuit ~ Normal
+#define N_DAISY32_F		0xF8D00		// Daisy Circuit ~ Last Lap
+#define N_DAISY32_N		0xF9960		// Daisy Circuit ~ Normal
+#define N_FARM_F		0x100118	// Moo Moo Meadows ~ Last Lap
+#define N_FARM_N		0xFD48C		// Moo Moo Meadows ~ Normal
+#define N_KINOKO_F		0xF9790		// Mushroom Gorge ~ Last Lap
+#define N_KINOKO_N		0xF97C8		// Mushroom Gorge ~ Normal
+#define N_MAPLE_F		0xFE370		// Maple Treeway ~ Last Lap
+#define N_MAPLE_N		0xF87AC		// Maple Treeway ~ Normal
+#define N_RAINBOW32_F	0xFED1C		// Rainbow Road ~ Last Lap
+#define N_RAINBOW32_N	0xFED98		// Rainbow Road ~ Normal
+#define N_RYUUSA_F		0xF9AA4		// Thwomp Desert ~ Last Lap
+#define N_RYUUSA_N		0xF9ADC		// Thwomp Desert ~ Normal
+#define N_SHOPPING32_F	0xFEFB4		// Coconut Mall ~ Last Lap
+#define N_SHOPPING32_N	0xFC5AC		// Coconut Mall ~ Normal
+#define N_SKATE_F		0xFC154		// Funky Stadium ~ Last minute
+#define N_SKATE_N		0xFD324		// Funky Stadium ~ Normal
+#define N_SNOWBOARD32_F	0xFDB30		// DK Snowboard Cross/DK Summit ~ Last Lap
+#define N_SNOWBOARD32_N	0xFC7D4		// DK Snowboard Cross/DK Summit ~ Normal
+#define N_VENICE_F		0xF9A34		// Delfino Pier ~ Last minute
+#define N_VENICE_N		0xF9A6C		// Delfino Pier ~ Normal
+#define N_VOLCANO32_F	0xFBE60		// Grumble Volcano ~ Last Lap
+#define N_VOLCANO32_N	0xFBE24		// Grumble Volcano ~ Normal
+
+
+/*==============================================================================*/
+/*================================ Miscanellous ================================*/
+/*==============================================================================*/
+#define O_CRS_IN_FAN			0xF775C		// Course Intro/Showcase
+#define O_CRS_IN_FAN_BATTLE		0xFB2C8		// Arena Intro/Showcase
+#define O_CRS_IN_FAN_MISSION	0xFD3EC		// Galaxy Colosseum Intro/Showcase (?)
+#define O_CRS_IN_FAN_WIFI		0xF816C		// Wi-Fi ~ Waiting for course to load
+#define O_ENDING_PARTA_32		0xFCE96		// Credits Part 1
+#define O_FANFAREBTDRAW_32		0xFA880		// Battle Results (Draw)
+#define O_FANFAREGP1_32			0xFFAA8		// Race Results (Win)
+#define O_FANFAREGP1_ONLY32		0xFAFA8		// Finish First (?)
+#define O_FANFAREGP2_32			0xFFAE4		// Race Results (2nd)
+#define O_FANFAREGP2_ONLY32		0xFAF68		// Finish 2nd (?)
+#define O_FANFAREGPDAME_32		0xF727C		// Race Results (<=3rd, ?)
+#define O_FANFAREGPDAME_ONLY32	0xF8738		
