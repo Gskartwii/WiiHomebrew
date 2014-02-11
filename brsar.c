2f9021cfeb775aa2f3fd1a0c507b726ae8dcb900
Gskartwii <agoldstar101@gmail.com>
2014-02-11 18:46:59 +0200

    Finished brsar.c

--- brsar.c
+++ brsar.c
@@ -4,6 +4,7 @@
 /*==============================================================================*/
 /*=========================== New Courses/Arenas ===============================*/
 /*==============================================================================*/
+// NOTICE: Multi-Channel BRSTMs are not here! They are on the bottom of the file!
 #define N_BLOCK_F		0xF929C		// Block Plaza ~ Last minute
 #define N_BLOCK_N		0xFE968		// Block Plaza ~ Normal
 #define N_BOSSMI_32		0xFFD80		// Galaxy Colosseum/Arena
@@ -49,4 +50,82 @@
 #define O_FANFAREGP2_32			0xFFAE4		// Race Results (2nd)
 #define O_FANFAREGP2_ONLY32		0xFAF68		// Finish 2nd (?)
 #define O_FANFAREGPDAME_32		0xF727C		// Race Results (<=3rd, ?)
-#define O_FANFAREGPDAME_ONLY32	0xF8738		
+#define O_FANFAREGPDAME_ONLY32	0xF8738		// Finish <=3rd (?)
+#define O_FANFAREMILOSE_32		0xFA950		// Battle Results (Lose)
+#define O_FANFAREMILOSE_ONLY32	0xFA120		// Lost a battle (?)
+#define O_FANFAREMIWIN_32		0xF862C		// Time Trial Results (Win)
+#define O_FANFAREMIWIN_ONLY32	0xF956C		// Win a Time Trial (?)
+#define O_FANFAREMIWINBOSS_32	0xFD740		// Win at Galaxy Colosseum (?)
+#define O_FANFARETA1ST_32		0xFD5E0		// Battle Results (Win, ?)
+#define O_FANFARETA1ST_ONLY32	0xFB624		// Win a battle (?)
+#define O_HYOUSYOU_CUT			0xF87E4		// Trophy Ceremony (short, ?)
+#define O_HYOUSYOU_LOSE			0xFFBB0		// No Trophy
+#define O_HYOUSYOU_NORMAL		0xFB668		// Trophy Ceremony (normal, ?)
+#define O_HYOUSYOU_WINNINGRUN	0xFF210		// Winning Run + Trophy Ceremony (long, ?)
+#define O_OPTION_32				0xFFCE8		// Menu
+#define O_START2_32_FAN			0xF8EF4		// Time Trial Countdown (?)
+#define O_START32_FAN			0xFC00C		// Race Countdown (?)
+#define O_WI_FI_WAITING32		0xFED58		// Ghost Replay / Wi-Fi waiting ~ Notice the BRSTM file name: o_Wi-Fi_waiting32.brstm
+
+/*==============================================================================*/
+/*============================= Retro Tracks/Arenas ============================*/
+/*==============================================================================*/
+#define R_64_BATTLE_F		0x100504	// N64 Skycraper ~ Last minute
+#define R_64_BATTLE_N		0xFF744		// N64 Skycraper ~ Normal
+#define R_64_CIRCUIT32_F	0xFA29C		// N64 Mario Raceway ~ Last Lap
+#define R_64_CIRCUIT32_N	0xFA260		// N64 Mario Raceway ~ Normal
+#define R_64_JUNGLE32_F		0xF7FE0		// N64 DK's Jungle Parkway ~ Last Lap
+#define R_64_JUNGLE32_N		0xFF254		// N64 DK's Jungle Parkway ~ Normal
+#define R_64_KUPPA32_F		0xFF7C0		// N64 Bowser's Castle ~ Last Lap
+#define R_64_KUPPA32_N		0xF86FC		// N64 Bowser's Castle ~ Normal
+#define R_64_SHERBET32_F	0xF93E4		// N64 Sherbet Land ~ Last Lap
+#define R_64_SHERBET32_N	0xF9420		// N64 Sherbet Land ~ Normal
+#define R_AGB_BATTLE_F		0x10017C	// GBA Battle Course 3 ~ Last minute
+#define R_AGB_BATTLE_N		0x1001B8	// GBA Battle Course 3 ~ Normal
+#define R_AGB_BEACH32_F		0xFC118		// GBA Shy Guy Beach ~ Last Lap
+#define R_AGB_BEACH32_N		0xF99F8		// GBA Shy Guy Beach ~ Normal
+#define R_AGB_KUPPA32_F		0xFC618		// GBA Bowser Castle 3 ~ Last Lap
+#define R_AGB_KUPPA32_N		0xFA574		// GBA Bowser Castle 3 ~ Normal
+#define R_DS_BATTLE_F		0xFCD00		// DS Twilight House ~ Last minute
+#define R_DS_BATTLE_N		0xFFC4C		// DS Twilight House ~ Normal
+#define R_DS_DESERT32_F		0xF90E0		// DS Desert Hills ~ Last Lap
+#define R_DS_DESERT32_N		0xFB95C		// DS Desert Hills ~ Normal
+#define R_DS_GARDEN32_F		0xFB3F8		// DS Peach Gardens ~ Last Lap
+#define R_DS_GARDEN32_N		0xF8C04		// DS Peach Gardens ~ Normal
+#define R_DS_JUNGLE32_F		0xF76B0		// DS Yoshi Falls ~ Last Lap
+#define R_DS_JUNGLE32_N		0xF9F34		// DS Yoshi Falls ~ Normal
+#define R_DS_TOWN32_F		0xFCCC4		// DS Delfino Square ~ Last Lap
+#define R_DS_TOWN32_N		0xFCE5C		// DS Delfino Square ~ Normal
+#define R_GC_BATTLE32_F		0xFBDE8		// GCN Cookie Land ~ Last minute
+#define R_GC_BATTLE32_N		0xFF9AC		// GCN Cookie Land ~ Normal
+#define R_GC_BEACH32_F		0xF77E4		// GCN Peach Beach ~ Last Lap
+#define R_GC_BEACH32_N		0xFA398		// GCN Peach Beach ~ Normal
+#define R_GC_CIRCUIT32_F	0xFAD9C		// GCN Mario Circuit ~ Last Lap
+#define R_GC_CIRCUIT32_N	0xFAD60		// GCN Mario Circuit ~ Normal
+#define R_GC_MOUNTAIN32_F	0xF95E0		// GCN DK Mountain ~ Last Lap
+#define R_GC_MOUNTAIN32_N	0xF9620		// GCN DK Mountain ~ Normal
+#define R_GC_STADIUM32_F	0xF8820		// GCN Waluigi Stadium ~ Last Lap
+#define R_GC_STADIUM32_N	0xF885C		// GCN Waluigi Stadium ~ Normal
+#define R_SFC_BATTLE_F		0xFA0E4		// SNES Battle Course 4 ~ Last minute
+#define R_SFC_BATTLE_N		0xFA0A8		// SNES Battle Course 4 ~ Normal
+#define R_SFC_CIRCUIT32_F	0xF6F6C		// SNES Mario Circuit 3 ~ Last Lap
+#define R_SFC_CIRCUIT32_N	0x1005D0	// SNES Mario Circuit 3 ~ Normal
+#define R_SFC_OBAKE32_F		0xF7548		// SNES Ghost Valley 2 ~ Last Lap
+#define R_SFC_OBAKE32_N		0xFEAA0		// SNES Ghost Valley 2 ~ Normal
+
+/*==============================================================================*/
+/*============================ Multi Channel BRSTMs ============================*/
+/*==============================================================================*/
+#define STRM_ME					0xFDC2C	// Unlocked Something
+#define STRM_N_DESERT_F			0xF8BC8	// Dry Dry Ruins ~ Last Lap
+#define STRM_N_DESERT_N			0xFA224	// Dry Dry Ruins ~ Normal
+#define STRM_N_FACTORY_F		0xFCF08	// Toad's Factory ~ Last Lap
+#define STRM_N_FACTORY_N		0xF750C	// Toad's Factory ~ Normal
+#define STRM_N_KOOPA_F			0xFA030	// Bowser's Castle ~ Last Lap
+#define STRM_N_KOOPA_N			0xFA06C	// Bowser's Castle ~ Normal
+#define STRM_N_RIDGEHIGHWAY_F	0xFF700	// Moonview Highway ~ Last Lap
+#define STRM_N_RIDGEHIGHWAY_N	0xFB7C8	// Moonview Highway ~ Normal
+#define STRM_N_TRUCK_F			0xF75E4	// Wario's Gold Mine ~ Last Lap
+#define STRM_N_TRUCK_N			0xFC9CC	// Wario's Gold Mine ~ Normal
+#define STRM_N_WATER_F			0xF8E28	// Koopa Cape ~ Last Lap
+#define STRM_N_WATER_N			0xFD8A4	// Koopa Cape ~ Normal
